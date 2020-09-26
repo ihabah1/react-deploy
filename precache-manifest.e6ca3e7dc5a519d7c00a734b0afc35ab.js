b63ba304940c8aaa756a34c7196410acce7943b9
ihabah1 <ihab.ah1@gmail.com>
2020-09-27 02:17:21 +0300

    Updates

---
+++ precache-manifest.e6ca3e7dc5a519d7c00a734b0afc35ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc95c2e6b342e31b94f",
+    "url": "/react-deploy/static/js/runtime~main.7cc95c2e.js"
+  },
+  {
+    "revision": "dfcd451b72098756b43a",
+    "url": "/react-deploy/static/js/main.dfcd451b.chunk.js"
+  },
+  {
+    "revision": "357814436f44ca4dda4f",
+    "url": "/react-deploy/static/js/2.35781443.chunk.js"
+  },
+  {
+    "revision": "c0d7ca8f2ac6fdf5215297200b625a56",
+    "url": "/react-deploy/index.html"
+  }
+];
